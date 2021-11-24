c3ab643c9ac7bee056e17186028bd48ffd3d0fec
devyosse <narutoshippuden884@gmail.com>
2021-11-24 19:51:16 +0100

    exo 79

--- exo.js
+++ exo.js
@@ -1,3 +1,26 @@
 let pomme = "Elle est belle ma pomme !";
 
 console.log(pomme.lastIndexOf('m'));
+
+
+let podex = pomme.indexOf('pomme')
+    document.getElementById('index-pomme').innerHTML = podex;
+
+
+let lastdex = pomme.lastIndexOf('m')
+document.getElementById('last-index-m').innerHTML = lastdex;
+
+if(pomme.startsWith("Elle")){
+    console.log("Elle est belle ma pomme ! Commence bien par Elle")
+}
+
+if(pomme.endsWith("!")){
+    console.log("Elle est belle ma pomme ! Se termine par un point d'exclamation")
+}
+
+let pomme2 = pomme.substring(18, 24);
+document.getElementById('pomme').innerHTML = pomme2;
+
+for (let i = 0; i <pomme.length;i++){
+    document.getElementById('one-line').innerHTML = pomme;
+}
